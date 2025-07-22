dcd399be9d1b7152bee9b697db656ab093cea717
VUALLAMM <brayan.sosa@ucic.edu.mx>
2025-07-22 03:45:02 -0600

    v1

---
+++ Assets/script/MovimientoJugador.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Vector2 entrada;
+    Rigidbody2D rb;
+    public float velocidad = 5f;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = velocidad * entrada;
+    }
+
+    public void Movimiento(InputAction.CallbackContext contexto)
+    {
+          
+        Vector2 valorEntrada = contexto.ReadValue<Vector2>();
+
+        animator.SetBool("estaCaminando", true);
+
+        // Determinar el eje dominante
+        if (Mathf.Abs(valorEntrada.x) > Mathf.Abs(valorEntrada.y))
+        {
+            // Movimiento horizontal
+            entrada = new Vector2(Mathf.Sign(valorEntrada.x), 0);
+        }
+        else if (Mathf.Abs(valorEntrada.y) > 0)
+        {
+            // Movimiento vertical
+            entrada = new Vector2(0, Mathf.Sign(valorEntrada.y));
+        }
+        else
+        {
+            entrada = Vector2.zero;
+        }
+        
+        //asignar valores a los parametros entraaX y entradaY
+        animator.SetFloat("entradaX", entrada.x);
+        animator.SetFloat("entradaY", entrada.y);
+
+        if(contexto.canceled){
+             animator.SetBool("estaCaminando", false);
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,68 @@
+\# Práctica de Motores de Videojuegos I - UCIC ðŸŽ®
+
+
+
+\## ðŸ§‘ Nombre del alumno/a:
+
+\*\*Nombre(s):\*\* \[Vuallamm]  
+
+\*\*Iniciales:\*\* \[V]
+
+
+
+\## ðŸ—‚ï¸ PrÃ¡ctica #
+
+\*\*TÃ­tulo de la prÃ¡ctica:\*\* \[Ej. Movimiento del personaje]  
+
+\*\*Fecha de entrega:\*\* \[24/06/2025]
+
+
+
+\## ðŸ§  DescripciÃ³n
+
+\[Explica brevemente en quÃ© consiste la prÃ¡ctica, quÃ© hiciste y quÃ© aprendiste. Ejemplo: En esta prÃ¡ctica implementÃ© el movimiento de un personaje 2D usando Rigidbody2D y controles de teclado en Unity.]
+
+
+
+\## ðŸŽ® Instrucciones de uso
+
+\[Describe cÃ³mo ejecutar el proyecto: versiÃ³n de Unity, plataforma objetivo, controles, etc.]
+
+
+
+Ejemplo:
+
+\- Unity versiÃ³n: 2022.3.x LTS
+
+\- Plataforma: PC
+
+\- Controles: Teclas de flecha para moverse, espacio para saltar
+
+
+
+\## ðŸ“ Estructura del proyecto
+
+\[Opcional: describe o muestra la estructura bÃ¡sica del proyecto, carpetas principales o assets relevantes.]
+
+
+
+\## ðŸ™Œ Créditos
+
+Este proyecto se desarrollÃ³ como parte de la materia \*\*Motores de Videojuegos I\*\* en la \*\*Universidad UCIC\*\*, durante el ciclo \[indicar ciclo, ej. Mayo-Agosto 2025].
+
+
+
+Se utilizaron las siguientes plantillas o recursos base:
+
+\- \[Nombre o descripciÃ³n de la plantilla o recurso] por \[cup_Nooble]
+
+\- \[Otro recurso, si aplica]
+
+
+
+---
+
+
+
+> \*Recuerda mantener tu repositorio ordenado y documentado. Â¡Buen trabajo! ðŸš€\*
+
